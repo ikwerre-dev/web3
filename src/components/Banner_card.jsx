696a782b96259c48ca06cf5199c7c8023709349a
Robinson Honour <investorhonour@gmail.com>
2024-05-03 21:02:40 +0100

    fixed the hot section on teh landing page

--- src/components/Banner_card.jsx
+++ src/components/Banner_card.jsx
@@ -1,54 +1,64 @@
 import React from 'react';
 import './Banner.css';
 import solana from '../assets/solana.png';
-import bannerAvatar from '../assets/banner-avatar.png';
+import showcaseData from './functions/useShowcaseData';
+import CountdownTimer from './functions/CountdownTimer'
+ 
 
 
 function BannerCard() {
-  return (
-   <div className='row'>
-    <div className='col-12 '>
-    <div className="card banner_card shadow pt-4 px-4 pb-1">
-      <div className='row'>
-        <div className='col-8'>
-            <div className='banner-card-avatar'>
-                <img src={bannerAvatar} alt='banner' className='banner-avatar' />
-                <img src={solana} alt='banner' className='banner-addon' />
-            </div>
-        </div>
-        <div className='col-4'>
-            <div className='badge mx-2'>
-            🔥  HOT DROP
-            </div>
-        </div>
-      </div>
-      <div className='mt-4'>
-        <h4>IranMan [ticker: IRAN]</h4>
-        <p className='text-muted text-m'>Market cap : 34.4K</p>
-      </div>
-      <p className='mt-2 fs-6 mb-0'>
-      Soft/Hard
-      </p>
 
-      <h4 className='text-main banner-amount-text'>
-      35,000 <span className='text-dard'>-</span> 140,000 SOL
-      </h4>
-      <hr className='mt-2'/>
-    
-    <div className='row mb-0'>
-        <div className='col-9'>
-            <div className='grid'>
-            <p className='mb-0'>Sale Starts In</p>
-            <p>07:13:56:36</p>
+  //this hot item part shows the item with hot value
+  const hotItems = showcaseData.filter(item => item.hot === 1);
+  const item = hotItems.length > 0 ? hotItems[0] : null;
+
+
+  return (
+
+    <div className='row' >
+      <div className='col-12 '>
+        <div className="card banner_card shadow pt-4 px-4 pb-1">
+          <div className='row'>
+            <div className='col-8'>
+              <div className='banner-card-avatar'>
+                <img src={item.backgroundImage} alt='banner' className='banner-avatar' />
+                <img src={solana} alt='banner' className='banner-addon' />
+              </div>
             </div>
+            <div className='col-4'>
+              <div className='badge mx-2'>
+                🔥  HOT DROP
+              </div>
+            </div>
+          </div>
+          <div className='mt-4'>
+            <h4>{item.ticker} [ticker: {item.tickerCode}]</h4>
+            <p className='text-muted text-m'>Market cap : {item.marketCap}K</p>
+          </div>
+          <p className='mt-2 fs-6 mb-0'>
+            Soft/Hard
+          </p>
+
+          <h4 className='text-main banner-amount-text'>
+            {item.minSol} <span className='text-dard'>-</span> {item.maxSol} {item.chain}
+          </h4>
+          <hr className='mt-2' />
+
+          <div className='row mb-0'>
+            <div className='col-9'>
+              <div className='grid'>
+                <p className='mb-0'>Sale Starts In</p>
+                <p><CountdownTimer saleStartTime={item.saleStartTime} /></p>
+              </div>
+            </div>
+            <div className='col-3'>
+              <a href={'details/' + item.id} className='btn btn-main mt-1 btn-sm'>View</a>
+            </div>
+          </div>
         </div>
-        <div className='col-3'>
-            <a href='details/1' className='btn btn-main mt-1 btn-sm'>View</a>
-        </div>
+      </div>
     </div>
-    </div>
-    </div>
-   </div>
+
   );
 }
 
--- src/components/functions/useShowcaseData.jsx
+++ src/components/functions/useShowcaseData.jsx
@@ -27,6 +27,7 @@ const showcaseData = Array.from({ length: 12 }, (v, i) => ({
     chain : randomFromArray(["SOL", "Blast", "Base Chain"]),
     PresaleType : randomFromRange(1, 2),
     presaleProgress : randomFromRange(1, 2),
+    hot : i+1,
     backgroundImage: require(`../../assets/banner/bg-${(i % 5) + 1}.jpg`), // Adjust if the number of backgrounds is less than 12
     ticker: ["IronMan", "Captain America", "Thor", "Hulk", "Black Widow", "Hawkeye", "Spider-Man", "Doctor Strange", "Ant-Man", "Wasp", "Falcon", "Winter Soldier"][i % 12],
     tickerCode: ["IRON", "CAP", "THOR", "HULK", "BWID", "HAWK", "SPID", "DRST", "ANTM", "WASP", "FALC", "WINT"][i % 12],
